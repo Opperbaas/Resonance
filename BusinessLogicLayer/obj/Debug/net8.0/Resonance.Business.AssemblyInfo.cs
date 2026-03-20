fab8951e1dde19ac4c8975549fd7f2f90a1efb18
Opperbaas <568270@student.fontys.nl>
2026-03-20 14:30:26 +0100

    Bump AssemblyInformationalVersion and rebuild

--- BusinessLogicLayer/obj/Debug/net8.0/Resonance.Business.AssemblyInfo.cs
+++ BusinessLogicLayer/obj/Debug/net8.0/Resonance.Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resonance.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143e35c7d085c073e75caa6529a4f956560aeb15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc0b52c38679a12245c049068ae018eb0c49af46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resonance.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resonance.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
